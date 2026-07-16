8aa549d565ca0ee00edd861e6ea6546f87e4dab5
wiscarce <15036023697@163.com>
2026-07-16 19:36:08 +0800

    modify train_config

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir gemma3-270mpip sh output
+
+pip install ms-swift
+
+git clone https://github.com/Wiscarce/gemma3-logo-homework.git
+
+git clone \
+https://github.com/roboticcam/logo-detailed-prompt
+
+modelscope download \
+--model google/gemma-3-270m-it \
+--local_dir ./gemma3-270m
+
+echo "Done!"
